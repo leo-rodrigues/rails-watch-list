0d9599e55dde871684c194ce2bdf19121c16c944
Leonardo Rodrigues <leonardomyfiis@gmail.com>
2021-10-22 17:01:21 -0300

    Feito models, seed, routes e criado controller vazio

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+Movie.delete_all
+
+movies_response = URI.open('http://tmdb.lewagon.com/movie/top_rated').read
+data = JSON.parse(movies_response)
+data['results'].each do |movie|
+  Movie.create!(
+    title: movie['original_title'],
+    overview: movie['overview'],
+    rating: movie['vote_average'],
+    poster_url: "https://image.tmdb.org/t/p/w500/#{movie['poster_path']}"
+  )
+end
+puts 'Done!'
