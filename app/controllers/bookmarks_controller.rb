9a68e15965179c8b36a3167cc49a5ab28954ca0b
Leonardo Rodrigues <leonardomyfiis@gmail.com>
2021-10-23 10:27:25 -0300

    Feito controller parcial

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,2 +1,25 @@
 class BookmarksController < ApplicationController
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new
+    if @bookmark.save
+      redirect_to list_path(@bookmark)
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:bookmark).permit(:comment)
+  end
 end
